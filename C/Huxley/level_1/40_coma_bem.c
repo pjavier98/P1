3090b987ee8d570f69b13dba09ad1ee183cfb6ad
pjavier98 <pjpc@ic.ufal.br>
2018-02-02 01:28:23 -0200

    2 pontos, 2 questoes

---
+++ C/Huxley/level_1/40_coma_bem.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float conta;         //conta + conta*10%
+    scanf("%f",&conta);
+    printf("%.2f",conta*0.1+conta);
+	return 0;
+}
