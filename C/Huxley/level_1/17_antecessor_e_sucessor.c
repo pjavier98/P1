800a7972bf5abddd1602ed44c3722d63a9a9a665
pjavier98 <pjpc@ic.ufal.br>
2018-02-02 01:07:33 -0200

    1 ponto, 1 questao

---
+++ C/Huxley/level_1/17_antecessor_e_sucessor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int num;      //antecessor e sucessor
+    scanf("%d",&num);
+    printf("%d %d",num-1,num+1);
+	return 0;
+}
