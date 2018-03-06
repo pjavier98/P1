dd2f2d34cc02b6bb8e9aaaeafa8a2406e3df9221
Pedro Javier <35467783+pjavier98@users.noreply.github.com>
2018-03-06 14:45:50 -0300

    Update 2050_Quadrado_Teatralnaya_funcao.c

--- C/Huxley/level_1/2050_Quadrado_Teatralnaya_funcao.c
+++ C/Huxley/level_1/2050_Quadrado_Teatralnaya_funcao.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
-int quadrado(int lado1, int lado2, int laje)
+int quadrado(int lado, int laje)
 {
   int altura = 0, largura = 0;
   
-  if (lado1 % laje == 0) 
+  if (lado % laje == 0) 
   {
-    largura = (lado1 / laje);
+    return (lado / laje);
   } 
   
   else 
   {
-    largura = ((lado1 / laje) + 1);
+    return ((lado / laje) + 1);
   }
   
-  if(lado2 % laje == 0) 
-  { 
-    altura = (lado2 / laje);
-  }
-  
-  else 
-  {
-    altura = ((lado2 / laje) + 1);
-  }
-  
-  return altura * largura;
 }
 
 int main() 
@@ -32,5 +21,10 @@ int main()
   
   scanf("%d %d %d", &lado1, &lado2, &laje);
   
-  printf("%d",quadrado(lado1, lado2, laje));
+
+  int qLado1 = quadrado(lado1, laje);
+
+  int qLado2 = quadrado(lado2, laje);
+
+  printf("%d", qLado1 * qLado2);
 }
